e52c11a308ab2f92d0fcae3e991f6f749cbcf745
Duc <congduc1199@gmail.com>
2020-02-24 16:51:56 +0700

    adfad

--- src/page/home/TrangMauThietke/TrangMauThietKe.js
+++ src/page/home/TrangMauThietke/TrangMauThietKe.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import PageTitleArea from "../../../components/PageTitleArea/PageTitleArea";
-import Subcribe from "../../../components/Subcribe/Subcribe";
-import Project from "../../../components/Project";
+import React, { Component } from 'react';
+import PageTitleArea from '../../../components/PageTitleArea/PageTitleArea';
+import Subcribe from '../../../components/Subcribe/Subcribe';
+import Project from '../../../components/Project';
 export default class TrangMauThietKe extends Component {
-  render() {
-    return (
-      <div>
-        <PageTitleArea title="Mẫu thiết kế" />
-        <Project />
-        <Subcribe />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<PageTitleArea title="Dự án" />
+				<Project />
+				<Subcribe />
+			</div>
+		);
+	}
 }
