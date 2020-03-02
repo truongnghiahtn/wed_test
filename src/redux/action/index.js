0d751b049b02d822eadd8e708f886077cca092a0
Duc <congduc1199@gmail.com>
2020-03-02 17:09:08 +0700

    ud 2/3 from duc

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -14,15 +14,15 @@ export const actGetListFeatureAPI = () => {
       .catch(err => console.log(err.response.data));
   };
 };
-export const getListService = () => {
+export const getListServiceApi = () => {
   return dispatch => {
-    CallAPI("service/api/find")
-      .then(rs => {
+    CallAPI("service/api/findAll")
+      .then(rs =>
         dispatch({
           type: Actiontype.GET_SERVICES,
-          listService: rs.data.data
-        });
-      })
+          listService: rs.data
+        })
+      )
       .catch(err => {
         console.log(err);
       });
@@ -58,89 +58,89 @@ export const actloginAdmin = (user, history) => {
 // end admin login
 
 export const getProjectsApi = () => {
-	return dispatch => {
-		CallAPI('project/api/findAll')
-			.then(rs => {
-				dispatch({
-					type: Actiontype.GET_PROJECTS,
-					dataProjects: rs.data,
-				});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+  return dispatch => {
+    CallAPI("project/api/findAll")
+      .then(rs => {
+        dispatch({
+          type: Actiontype.GET_PROJECTS,
+          dataProjects: rs.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 };
 
 export const getPricesApi = () => {
-	return dispatch => {
-		CallAPI('price/api/findAll')
-			.then(rs => {
-				dispatch({
-					type: Actiontype.GET_PRICES,
-					dataPrices: rs.data,
-				});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+  return dispatch => {
+    CallAPI("price/api/findAll")
+      .then(rs => {
+        dispatch({
+          type: Actiontype.GET_PRICES,
+          dataPrices: rs.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 };
 
 export const getBlogApi = () => {
-	return dispatch => {
-		CallAPI('blog/api/findAll')
-			.then(rs => {
-				dispatch({
-					type: Actiontype.GET_BLOG,
-					dataBlog: rs.data,
-				});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+  return dispatch => {
+    CallAPI("blog/api/findAll")
+      .then(rs => {
+        dispatch({
+          type: Actiontype.GET_BLOG,
+          dataBlog: rs.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 };
 export const actGetListTeamAPI = () => {
-	return dispatch => {
-		CallAPI(`team/api/findAll`, 'GET', null, null)
-			.then(res =>
-				dispatch({
-					type: Actiontype.GET_LIST_TEAM,
-					team: res,
-				})
-			)
-			.catch(err => console.log(err.response.data));
-	};
+  return dispatch => {
+    CallAPI(`team/api/findAll`, "GET", null, null)
+      .then(res =>
+        dispatch({
+          type: Actiontype.GET_LIST_TEAM,
+          team: res
+        })
+      )
+      .catch(err => console.log(err.response.data));
+  };
 };
 
 export const actGetInfoCompanyAPI = () => {
-	return dispatch => {
-		CallAPI(`company/api/findAll`, 'GET', null, null)
-			.then(res =>
-				dispatch({
-					type: Actiontype.GET_INFO_COMPANY,
-					company: res,
-				})
-			)
-			.catch(err => console.log(err.response.data));
-	};
+  return dispatch => {
+    CallAPI(`company/api/findAll`, "GET", null, null)
+      .then(res =>
+        dispatch({
+          type: Actiontype.GET_INFO_COMPANY,
+          company: res
+        })
+      )
+      .catch(err => console.log(err.response.data));
+  };
 };
 
 export const actPostInfoCustomerAPI = data => {
-	return dispatch => {
-		CallAPI(`customer/api/create`, 'POST', data, null)
-			.then(res =>
-				dispatch(
-					{
-						type: Actiontype.POST_INFO_CUSTOMER,
-						customer: data,
-					},
-					console.log(data)
-				)
-			)
-			.catch(err => console.log(err.response.data));
-	};
+  return dispatch => {
+    CallAPI(`customer/api/create`, "POST", data, null)
+      .then(res =>
+        dispatch(
+          {
+            type: Actiontype.POST_INFO_CUSTOMER,
+            customer: data
+          },
+          console.log(data)
+        )
+      )
+      .catch(err => console.log(err.response.data));
+  };
 };
 // admin team
 export const editTeam = data => {
@@ -154,28 +154,22 @@ export const actOnEdit = () => {
   };
 };
 export const actPostTeam = data => {
-  return dispatch=>{
+  return dispatch => {
     CallAPI(`team/api/create`, "POST", data, null)
-      .then(res =>{
-        console.log(res)
-       
-      }
-      )
+      .then(res => {
+        console.log(res);
+      })
       .catch(err => console.log(err.response.data));
-  }
-    
+  };
 };
 export const actPutTeam = data => {
-  return dispatch=>{
+  return dispatch => {
     CallAPI(`team/api/update`, "PUT", data, null)
-      .then(res =>{
-        console.log(res)
-        
-      }
-      )
+      .then(res => {
+        console.log(res);
+      })
       .catch(err => console.log(err.response.data));
-  }
-    
+  };
 };
 // customer
 export const actGetCustomer = () => {
@@ -191,4 +185,15 @@ export const actGetCustomer = () => {
   };
 };
 
-
+export const getCategoryProjectsApi = () => {
+  return dispatch => {
+    CallAPI(`category-project/api/findAll`, "GET", null, null)
+      .then(res =>
+        dispatch({
+          type: Actiontype.GET_CATEGORY_PROJECTS,
+          dataCategoryProjects: res.data
+        })
+      )
+      .catch(err => console.log(err.response.data));
+  };
+};
