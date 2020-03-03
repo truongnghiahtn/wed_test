b909743d32a75b8d707505052c68dcfa9a33f57d
Duc <congduc1199@gmail.com>
2020-03-03 21:19:22 +0700

    sdfasd

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,140 +1,77 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import { connect } from 'react-redux';
-import * as actions from '../../redux/action/index';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../redux/action/index";
 class Pricing extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [
-				{
-					title: 'Cơ Bản',
-					gia: 24,
-					moTa: ['Chọn mẫu và chỉnh sửa', 'Tặng tên miền Quốc tế', 'Dung lượng: 1200MB', 'Băng thông: 60Gb'],
-				},
-				{
-					title: 'Tiêu chuẩn',
-					gia: 59,
-					moTa: [
-						'Chỉnh sửa theo yêu cầu (02 lần)',
-						'Tặng tên miền Quốc tế',
-						'Dung lượng: 2800MB',
-						'Băng thông: 200Gb',
-					],
-				},
-				{
-					title: 'Cao Cấp',
-					gia: 89,
-					moTa: [
-						'Chỉnh sửa theo yêu cầu (05 lần)',
-						'Tặng tên miền Quốc tế',
-						'Dung lượng: 4500MB',
-						'Băng thông: Không giới hạn',
-					],
-				},
-			],
-			className: ['bg-f6f5fb', 'bg-ed0678', 'bg-edfbf8'],
-		};
-	}
-	componentDidMount() {
-		this.props.getPricesApi();
-	}
-	renderPricingWebsite = isMonth => {
-		console.log(this.props.dataPrices);
-		return this.state.data.map((item, index) => {
-			return (
-				<div className="col-lg-4 col-md-6 col-sm-6" key={index}>
-					<div className={classnames(`single-pricing-box ${this.state.className[index]}`)}>
-						<div className="pricing-header">
-							<h3>{item.title}</h3>
-						</div>
-						<div className="price">
-							<strong>$</strong>
-							<span>{isMonth ? item.gia : item.gia * 10}</span>
-						</div>
-						<ul className="pricing-features">
-							{item.moTa.map((item2, index2) => {
-								return (
-									<li key={index2}>
-										<i className="flaticon-check-mark" />
-										{item2}
-									</li>
-								);
-							})}
-						</ul>
-						<div className="price-btn">
-							<a href="#" className="price-btn-one">
-								Bắt đầu
-							</a>
-						</div>
-					</div>
-				</div>
-			);
-		});
-	};
-	render() {
-		return (
-			<section className="pricing-section ptb-100">
-				<div className="container">
-					<div className="section-title">
-						<span>Bảng giá</span>
-						<h3>Bảng giá thiết kế website</h3>
-					</div>
-					<div className="tab pricing-tab">
-						<ul className="nav tabs" id="myTab" role="tablist">
-							<li className="nav-item">
-								<a
-									className="nav-link active"
-									id="month-tab"
-									data-toggle="tab"
-									href="#month"
-									role="tab"
-								>
-									Tháng
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" id="year-tab" data-toggle="tab" href="#year" role="tab">
-									Năm
-								</a>
-							</li>
-						</ul>
-						<div className="tab-content" id="myTabContent">
-							<div
-								className="tab-pane fade show active"
-								id="month"
-								role="tabpanel"
-								aria-labelledby="month-tab"
-							>
-								<div className="tabs-item">
-									<div className="row">{this.renderPricingWebsite(true)}</div>
-								</div>
-							</div>
-							<div className="tab-pane fade" id="year" role="tabpanel" aria-labelledby="year-tab">
-								<div className="tabs-item">
-									<div className="row">{this.renderPricingWebsite(false)}</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		);
-	}
+  componentDidMount() {
+    this.props.getPricesApi();
+  }
+  renderPricingWebsite = () => {
+    console.log(this.props.dataPrices);
+    return this.props.dataPrices.map((item, index) => {
+      return (
+        <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
+          <div className="single-pricing-box bg-f6f5fb">
+            <div className="price">
+              <strong>$</strong>
+              <span>{item.price}</span>
+            </div>
+            <p>{item.content_price}</p>
+            <ul className="pricing-features">
+              <li>
+                <i className="flaticon-check-mark" />
+                <strong>Dịch vụ: {item.name_service.name_service}</strong>
+              </li>
+              <li>Mô tả: {item.name_service.content_service}</li>
+              <li>
+                <i className="flaticon-check-mark" />
+                <strong>
+                  Loại dịch vụ:{" "}
+                  {item.name_service.category_service
+                    ? item.name_service.category_service.name_category_service
+                    : "unknow"}
+                </strong>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    });
+  };
+  render() {
+    return (
+      <section className="pricing-section ptb-100">
+        <div className="container">
+          <div className="section-title">
+            <span>Bảng giá</span>
+            <h3>Bảng giá thiết kế website</h3>
+          </div>
+          <div className="tab pricing-tab">
+            <div className="tab-content">
+              <div className="tab-pane fade show active">
+                <div className="tabs-item">
+                  <div className="row">{this.renderPricingWebsite()}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export const mapStateToProps = state => {
-	return {
-		dataPrices: state.deMoReducer.dataPrices,
-	};
+  return {
+    dataPrices: state.deMoReducer.dataPrices
+  };
 };
 
 export const mapDispatchToProps = dispatch => {
-	return {
-		getPricesApi: () => {
-			dispatch(actions.getPricesApi());
-		},
-	};
+  return {
+    getPricesApi: () => {
+      dispatch(actions.getPricesApi());
+    }
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Pricing);
